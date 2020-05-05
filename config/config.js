074bd22438c6dcbc2642742fc461e3f6f8dc0b52
Jason Hodges <jasonhodges0845@gmail.com>
2020-05-05 15:53:10 -0500

    prep for heroku deployment stage 2

--- config/config.js
+++ config/config.js
@@ -23,18 +23,6 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "operatorsAliases": false,
-
-    "pool":{
-      "max": 5,
-      "min": 0,
-      "acquire": 30000,
-      "idle": 10000
-    }
+    "use_env_variable": "DATABASE_URL"
   }
 }
